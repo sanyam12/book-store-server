f3fefe9cdb2b68cecd4a052e3f5ed0fc4411d5bb
Sanyam Ratreja <sanyamratreja18@gmail.com>
2023-09-02 23:26:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const admin = require("firebase-admin");
+const serviceAccount = require("./admin.json");
+
+// var { doc, getDoc } = require("firebase/firestore");
+// var getFirestore = require("firebase/firestore").getFirestore;
+// var initializeApp = require("@firebase/app").initializeApp;
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDqdUqwtZ23FuPetbnq_Kv0PgwdYYObICc",
+//     authDomain: "book-store-193be.firebaseapp.com",
+//     projectId: "book-store-193be",
+//     storageBucket: "book-store-193be.appspot.com",
+//     messagingSenderId: "517541915143",
+//     appId: "1:517541915143:web:e8b6fbe6aae35b4cac98a2",
+//     measurementId: "G-J7M7869SFD"
+// };
+
+// var firebaseApp = initializeApp(firebaseConfig);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://your-firebase-project-id.firebaseio.com', // Replace with your Firestore database URL
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js Server!');
+});
+
+app.post("/uploadBook", (req, res) => {
+    const jsonData = req.body;
+
+    // Check if JSON data exists
+    if (jsonData["name"] === undefined || jsonData["price"] === undefined || jsonData["link"] === undefined) {
+        return res.status(400).json({ error: 'JSON data provided is not complete' });
+    }
+
+    var db = admin.firestore();
+    var ref = db.collection('books').doc(jsonData["name"]).set({
+        name: jsonData["name"].toString(),
+        price: jsonData["price"].toString(),
+        link: jsonData["link"].toString(),
+        pages: jsonData["pages"],
+        description:jsonData["description"].toString(),
+    }).then(() => {
+        console.log(`Added document with ID: ${jsonData["name"]}`);
+    }).catch((err) => {
+        console.log('Error getting documents', err);
+    });
+    res.send("Upload Book");
+});
+
+app.get("/getBook", async (req, res) => {
+    var db = admin.firestore();
+    const docRef = await db.collection('books').doc(req.query.name).get();
+    if (!docRef.exists) {
+        res.status(404);
+        res.send("No such document!");
+    } else {
+        var temp = docRef.data();
+        res.send(temp);
+    }
+});
+
+app.get("/getBooks", async (req, res) => {
+    const db = admin.firestore();
+    const collectionRef = await db.collection('books').get();
+    const data = collectionRef.docs.map(doc => doc.data());
+    res.send(data);
+});
+
+app.post("/saveToLibrary", async (req, res) => {
+    var json = req.body;
+    var db = admin.firestore();
+    console.log(json["book"]["name"]);
+    var ref = db.collection('users').doc(json["uid"]).collection('library').doc(json["book"]["name"])
+    .set(json["book"])
+    .then(() => {
+        console.log(`Added document with ID: ${json["book"]["name"]}`);
+    }).catch((err) => {
+        console.log('Error getting documents', err);
+    });
+    res.send("Saved to Library");
+});
+
+app.post("/getLibrary", async (req, res) => {
+    var json = req.body;
+    var db = admin.firestore();
+    const collectionRef = await db.collection('users').doc(json["uid"]).collection('library').get();
+    const data = collectionRef.docs.map(doc => doc.data());
+    res.send(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
